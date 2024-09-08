d096fe30f066c08ae59c3c2a3075b22987629c37
ibrahim dursun <ibrahim@dursun.cc>
2024-09-08 17:33:34 +0100

    remove main.go

--- cmd/test1.go
+++ cmd/test1.go
@@ -7,7 +7,6 @@ import (
 )
 
 func main() {
-	//commits := jj.GetCommits("/Users/idursun/repositories/elixir/beach_games")
 	commits := jj.GetCommits(os.Getenv("PWD"))
 	for _, commit := range commits {
 		fmt.Printf("%+v\n", commit)
