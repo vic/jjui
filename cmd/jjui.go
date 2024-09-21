626736ccb460e67b9dddc65778efa372db07c94d
ibrahim dursun <ibrahim@dursun.cc>
2024-09-21 21:18:51 +0100

    refactor: drop compositor

--- cmd/jjui.go
+++ cmd/jjui.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	tea "github.com/charmbracelet/bubbletea"
-	"jjui/internal/ui"
 	"os"
+
+	"jjui/internal/ui/revisions"
+
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	p := tea.NewProgram(ui.New())
+	p := tea.NewProgram(revisions.New())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error running program: %v\n", err)
 		os.Exit(1)
