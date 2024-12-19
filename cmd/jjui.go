3b3a30d6ee32d8622538409bd4de14b1c589666a
ibrahim dursun <ibrahim@dursun.cc>
2024-12-19 19:57:10 +0000

    feat: print version when ran with `--version` argument

--- cmd/jjui.go
+++ cmd/jjui.go
@@ -10,6 +10,10 @@ import (
 var Version = "unknown"
 
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Printf("jjui version %s\n", Version)
+		os.Exit(0)
+	}
 	p := tea.NewProgram(ui.New(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Error running program: %v\n", err)
