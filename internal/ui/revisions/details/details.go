1d81c200ca4555bf9c04fe33c82b165adcd03ad8
ibrahim dursun <ibrahim@dursun.cc>
2025-01-20 23:46:26 +0000

    refactor: use list component for rendering changed files

--- internal/ui/revisions/details/details.go
+++ internal/ui/revisions/details/details.go
@@ -1,31 +1,68 @@
 package details
 
 import (
+	"fmt"
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"io"
 	"jjui/internal/ui/common"
 	"strings"
 )
 
 var (
-	Added            = lipgloss.NewStyle().Foreground(common.Green)
-	AddedSelected    = lipgloss.NewStyle().Inherit(Added).Bold(true).Background(common.DarkBlack)
-	Deleted          = lipgloss.NewStyle().Foreground(common.Red)
-	DeletedSelected  = lipgloss.NewStyle().Inherit(Deleted).Bold(true).Background(common.DarkBlack)
-	Modified         = lipgloss.NewStyle().Foreground(common.Cyan)
-	ModifiedSelected = lipgloss.NewStyle().Inherit(Modified).Bold(true).Background(common.DarkBlack)
+	Added    = lipgloss.NewStyle().Foreground(common.Green)
+	Deleted  = lipgloss.NewStyle().Foreground(common.Red)
+	Modified = lipgloss.NewStyle().Foreground(common.Cyan)
 )
+
+type item string
+
+func (f item) Title() string       { return string(f) }
+func (f item) Description() string { return "" }
+func (f item) FilterValue() string { return string(f) }
+
+type itemDelegate struct{}
+
+func (i itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	f, ok := listItem.(item)
+	if !ok {
+		return
+	}
+	file := string(f)
+
+	var style = Modified
+	if strings.HasPrefix(file, "A") {
+		style = Added
+	} else if strings.HasPrefix(file, "D") {
+		style = Deleted
+	}
+	if index == m.Index() {
+		style = style.Bold(true).Background(common.DarkBlack)
+	}
+	fmt.Fprint(w, style.Render(file))
+}
+
+func (i itemDelegate) Height() int                         { return 1 }
+func (i itemDelegate) Spacing() int                        { return 0 }
+func (i itemDelegate) Update(tea.Msg, *list.Model) tea.Cmd { return nil }
 
 type Model struct {
 	revision string
-	selected int
-	files    []string
+	files    list.Model
 	common.Commands
 }
 
 func New(revision string, commands common.Commands) tea.Model {
+	l := list.New(nil, itemDelegate{}, 0, 0)
+	l.SetFilteringEnabled(false)
+	l.SetShowTitle(false)
+	l.SetShowStatusBar(false)
+	l.SetShowPagination(false)
+	l.SetShowHelp(false)
 	return Model{
 		revision: revision,
+		files:    l,
 		Commands: commands,
 	}
 }
@@ -41,50 +78,24 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "esc", "h":
 			return m, common.Close
 		case "d":
-			return m, m.Commands.GetDiff(m.revision, m.files[m.selected][2:])
-		case "up", "k":
-			if m.selected > 0 {
-				m.selected--
-			}
-		case "down", "j":
-			if m.selected < len(m.files)-1 {
-				m.selected++
-			}
+			v := m.files.SelectedItem().FilterValue()
+			return m, m.Commands.GetDiff(m.revision, v[2:])
+		default:
+			var cmd tea.Cmd
+			m.files, cmd = m.files.Update(msg)
+			return m, cmd
 		}
 	case common.UpdateCommitStatusMsg:
-		m.files = msg
+		items := make([]list.Item, len(msg))
+		for i, status := range msg {
+			items[i] = item(status)
+		}
+		m.files.SetItems(items)
+		m.files.SetHeight(min(10, len(items)))
 	}
 	return m, nil
 }
 
 func (m Model) View() string {
-	var w strings.Builder
-	maxWidth := 0
-	for _, status := range m.files {
-		if len(status) > maxWidth {
-			maxWidth = len(status)
-		}
-	}
-	for i, status := range m.files {
-		style := Modified
-		if strings.HasPrefix(status, "M") {
-			style = Modified
-			if m.selected == i {
-				style = ModifiedSelected
-			}
-		} else if strings.HasPrefix(status, "D") {
-			style = Deleted
-			if m.selected == i {
-				style = DeletedSelected
-			}
-		} else {
-			style = Added
-			if m.selected == i {
-				style = AddedSelected
-			}
-		}
-		w.WriteString(style.Width(maxWidth).Render(status))
-		w.WriteString("\n")
-	}
-	return w.String()
+	return m.files.View()
 }
