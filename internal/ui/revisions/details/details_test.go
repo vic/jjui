dc75994ffe63c2e7d2a18c59b411753b7686ee5b
ibrahim dursun <ibrahim@dursun.cc>
2025-02-05 09:04:07 +0000

    refactor: move test.shellModel into test

--- internal/ui/revisions/details/details_test.go
+++ internal/ui/revisions/details/details_test.go
@@ -15,27 +15,6 @@ const (
 	StatusOutput = "M file.txt\nA newfile.txt\n"
 )
 
-type model struct {
-	details tea.Model
-}
-
-func (m model) Init() tea.Cmd {
-	return m.details.Init()
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	if _, ok := msg.(common.CloseViewMsg); ok {
-		return m, tea.Quit
-	}
-	var cmd tea.Cmd
-	m.details, cmd = m.details.Update(msg)
-	return m, cmd
-}
-
-func (m model) View() string {
-	return m.details.View()
-}
-
 func TestModel_Init_ExecutesStatusCommand(t *testing.T) {
 	commands := test.NewJJCommands()
 	statusCommand := commands.ExpectStatus(t, Revision)
@@ -57,42 +36,14 @@ func TestModel_Update_RestoresSelectedFiles(t *testing.T) {
 
 	commands.ExpectRestore(t, Revision, []string{"file.txt"})
 
-	tm := teatest.NewTestModel(t, New(Revision, common.NewUICommands(commands)))
+	tm := teatest.NewTestModel(t, test.NewShell(New(Revision, common.NewUICommands(commands))))
 	teatest.WaitFor(t, tm.Output(), func(bts []byte) bool {
 		return bytes.Contains(bts, []byte("file.txt"))
 	})
 
 	tm.Send(tea.KeyMsg{Type: tea.KeySpace})
 	tm.Send(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune("r")})
-	teatest.WaitFor(t, tm.Output(), func(bts []byte) bool {
-		return bytes.Contains(bts, []byte("Yes"))
-	})
 	tm.Send(tea.KeyMsg{Type: tea.KeyEnter})
-	teatest.WaitFor(t, tm.Output(), func(bts []byte) bool {
-		return bytes.Contains(bts, []byte("file.txt"))
-	})
-	tm.Quit()
-	tm.WaitFinished(t, teatest.WithFinalTimeout(3*time.Second))
-	commands.Verify(t)
-}
-
-func TestModel_Update_SplitsSelectedFiles(t *testing.T) {
-	commands := test.NewJJCommands()
-	statusCommand := commands.ExpectStatus(t, Revision)
-	statusCommand.Output = []byte(StatusOutput)
-
-	commands.ExpectSplit(t, Revision, []string{"file.txt"})
-	shell := model{
-		details: New(Revision, common.NewUICommands(commands)),
-	}
-
-	tm := teatest.NewTestModel(t, shell)
-	teatest.WaitFor(t, tm.Output(), func(bts []byte) bool {
-		return bytes.Contains(bts, []byte("file.txt"))
-	})
-
-	tm.Send(tea.KeyMsg{Type: tea.KeySpace})
-	tm.Send(tea.KeyMsg{Type: tea.KeyRunes, Runes: []rune("s")})
 	tm.WaitFinished(t, teatest.WithFinalTimeout(3*time.Second))
 	commands.Verify(t)
 }
---
+++ test/shell.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"jjui/internal/ui/common"
+)
+
+type model struct {
+	embeddedModel tea.Model
+}
+
+func (m model) Init() tea.Cmd {
+	return m.embeddedModel.Init()
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var shellCmd tea.Cmd
+	if _, ok := msg.(common.CloseViewMsg); ok {
+		shellCmd = tea.Quit
+	}
+	var cmd tea.Cmd
+	m.embeddedModel, cmd = m.embeddedModel.Update(msg)
+	return m, tea.Sequence(cmd, shellCmd)
+}
+
+func (m model) View() string {
+	return m.embeddedModel.View()
+}
+
+func NewShell(details tea.Model) tea.Model {
+	return model{
+		embeddedModel: details,
+	}
+}
