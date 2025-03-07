9e74623efcec41f0bb68c9dddc0ce1e2ef27bb30
ibrahim dursun <ibrahim@dursun.cc>
2025-03-07 23:17:41 +0000

    fix(revisions): remove loading message if there are no revisions to display

--- internal/ui/revisions/revisions.go
+++ internal/ui/revisions/revisions.go
@@ -228,7 +228,7 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m *Model) View() string {
-	if len(m.rows) == 0 {
+	if m.rows == nil {
 		return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, "loading")
 	}
 	topView := ""
