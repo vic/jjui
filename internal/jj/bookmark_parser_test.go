a3fbd01b0ff3ce6a587d6c21a6041b2fb9fcd926
ibrahim dursun <ibrahim@dursun.cc>
2025-05-21 09:53:19 +0100

    test: add bookmark_parser test

---
+++ internal/jj/bookmark_parser_test.go
@@ -0,0 +1,62 @@
+package jj
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseBookmarkListOutput(t *testing.T) {
+	type args struct {
+		output string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []Bookmark
+	}{
+		{
+			name: "empty",
+			args: args{
+				output: "",
+			},
+			want: nil,
+		},
+		{
+			name: "single",
+			args: args{
+				output: "feat-1;false;false;false;false;9",
+			},
+			want: []Bookmark{
+				{
+					Name:      "feat-1",
+					Tracked:   false,
+					Remote:    false,
+					Conflict:  false,
+					Backwards: false,
+					CommitId:  "9",
+				},
+			},
+		},
+		{
+			name: "remote",
+			args: args{
+				output: "feature@origin;true;true;false;false;b",
+			},
+			want: []Bookmark{
+				{
+					Name:      "feature@origin",
+					Tracked:   true,
+					Remote:    true,
+					Conflict:  false,
+					Backwards: false,
+					CommitId:  "b",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, ParseBookmarkListOutput(tt.args.output), "ParseBookmarkListOutput(%v)", tt.args.output)
+		})
+	}
+}
