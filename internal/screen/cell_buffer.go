80b87cea1cdee23164d9ffe34565074cd61d3ff9
ibrahim dursun <ibrahim@dursun.cc>
2025-06-17 09:27:31 +0100

    fix: support CJK encoding
    fixes #130

--- internal/screen/cell_buffer.go
+++ internal/screen/cell_buffer.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"strings"
 
+	"github.com/mattn/go-runewidth"
 	"github.com/rivo/uniseg"
 )
 
@@ -58,7 +59,28 @@ func (b *cellBuffer) applyANSI(input []byte, offsetX, offsetY int) {
 				Text:   cluster,
 				Params: st.Params,
 			}
+
+			width := runewidth.StringWidth(cluster)
 			currentCol++
+
+			// For double-width characters (like CJK), we need to add an empty cell
+			// to prevent the next character from overlapping
+			if width > 1 {
+				if currentCol < len(b.grid[currentLine]) {
+					// Mark the next cell as occupied by making it a zero-width space
+					b.grid[currentLine][currentCol] = Segment{
+						Text:   "", // Empty cell as placeholder
+						Params: st.Params,
+					}
+				} else {
+					// Need to expand for the placeholder
+					b.grid[currentLine] = append(b.grid[currentLine], Segment{
+						Text:   "",
+						Params: st.Params,
+					})
+				}
+				currentCol++
+			}
 		}
 	}
 }
